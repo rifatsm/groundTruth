bf528c17e30da776ebe13114fd3a64e537e3f0c0
GroundTruth <groundtruth@Kurts-iMac.local>
2017-12-01 17:50:16 -0500

    Search Box added on the Designate pages

--- ground_truth/static/ground_truth/js/designate.js
+++ ground_truth/static/ground_truth/js/designate.js
@@ -650,8 +650,73 @@ function initMap() {
 
     });
 
+
     view_tracker();
     zoom_style_tracker();
+
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    console.log(searchBox);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      console.log("inside listener");
+      console.log("map: "+map.getBounds());
+      console.log("searchBox: "+searchBox);
+      var test = searchBox.setBounds(map.getBounds());
+      console.log("test: "+test);
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+    // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
 
     //Create the Drawing manager which will handle the designation
     var drawingManager = new google.maps.drawing.DrawingManager({
